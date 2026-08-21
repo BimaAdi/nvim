75ba187d19e549832ea732d6da957a1b49dc2b58
BimaAdi <bimaadi419@gmail.com>
2026-08-21 17:30:39 +0700

    add buffeline, lualine and treesitter

--- init.lua
+++ init.lua
@@ -8,3 +8,8 @@ vim.opt.shiftwidth = 2   -- Size of an indent
 vim.opt.softtabstop = 2  -- Number of spaces a <Tab> counts for while performing editing operations
 vim.opt.expandtab = true -- Convert tabs to spaces
 
+-- buffer
+vim.keymap.set('n', '<C-w>', ':bd<cr>', { desc = 'close current buffer' })
+vim.keymap.set('n', '<C-d>', ':bn<cr>', { desc = 'go to next buffer' })
+vim.keymap.set('n', '<C-a>', ':bp<cr>', { desc = 'go to prev buffer' })
+
