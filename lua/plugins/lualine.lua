e398da685dac1d7106d59f50d5c209f1e85f9ed6
BimaAdi <bimaadi419@gmail.com>
2026-08-27 06:58:07 +0700

    add html auto close and filepath on lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,9 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("lualine").setup()
+    require("lualine").setup({
+      options = { "filename", path = 1 }
+    })
     vim.opt.showmode = false -- to disable default showmode (normal/insert/visual)
   end
 }
